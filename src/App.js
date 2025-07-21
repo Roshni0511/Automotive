16b8ecdbb0e0b2f31b15a284946245d156522b77
Roshni0511 <roshnichauhan051@gmail.com>
2025-07-21 15:35:39 +0530

    set

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from 'react';
+import WOW from 'wowjs';
 import Home from "./pages/Home";
 
 
 
 
 function App() {
+    useEffect(() => {
+  new WOW.WOW().init();
+}, []);
   return (
 <>
 <Routes>
   <Route path="/" element={<Home />}/>
 </Routes>
 
-
+  <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
 </>
   );
 }
