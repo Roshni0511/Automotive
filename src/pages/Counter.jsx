7753914f8a7b4ced11aefa552f34d22653cf6420
Roshni0511 <roshnichauhan051@gmail.com>
2025-07-21 17:58:29 +0530

    set

--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -5,7 +5,7 @@ export default function Counter() {
         const counters = document.querySelectorAll('[data-toggle="counter-up"]');
         counters.forEach((el) => {
           counterUp(el, {
-            duration: 2000,
+            duration: 4000,
             delay: 10,
           });
         });
@@ -17,23 +17,23 @@ export default function Counter() {
         <div className="row g-4">
           <div className="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.1s">
             <i className="fa fa-check fa-2x text-white mb-3"></i>
-            <h2 className="text-white mb-2" data-toggle="counter-up">1234</h2>
+            <h2 className="text-white mb-2" data-toggle="counter-up">20+</h2>
             <p className="text-white mb-0">Years Experience</p>
           </div>
           <div className="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.3s">
             <i className="fa fa-users-cog fa-2x text-white mb-3"></i>
-            <h2 className="text-white mb-2" data-toggle="counter-up">1234</h2>
-            <p className="text-white mb-0">Expert Technicians</p>
+            <h2 className="text-white mb-2" data-toggle="counter-up">990+</h2>
+            <p className="text-white mb-0">Tons Of Customers</p>
           </div>
           <div className="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.5s">
             <i className="fa fa-users fa-2x text-white mb-3"></i>
-            <h2 className="text-white mb-2" data-toggle="counter-up">1234</h2>
-            <p className="text-white mb-0">Satisfied Clients</p>
+            <h2 className="text-white mb-2" data-toggle="counter-up">100+</h2>
+            <p className="text-white mb-0">Modern Equipments</p>
           </div>
           <div className="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.7s">
             <i className="fa fa-car fa-2x text-white mb-3"></i>
-            <h2 className="text-white mb-2" data-toggle="counter-up">1234</h2>
-            <p className="text-white mb-0">Complete Projects</p>
+            <h2 className="text-white mb-2" data-toggle="counter-up">500+</h2>
+            <p className="text-white mb-0">Vehicles Service Per Month</p>
           </div>
         </div>
       </div>
