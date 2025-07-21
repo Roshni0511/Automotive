c7d524ce4e7a1d9ceec141a52924258dfb29b0e8
Roshni0511 <roshnichauhan051@gmail.com>
2025-07-21 15:36:50 +0530

    set

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -381,73 +381,73 @@ export default function Home() {
     {/* <!-- Booking End --> */}
 
      {/* <!-- Team Start --> */}
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="text-primary text-uppercase">// Our Technicians //</h6>
-                <h1 class="mb-5">Our Expert Technicians</h1>
+    <div className="container-xxl py-5">
+        <div className="container">
+            <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <h6 className="text-primary text-uppercase">// Our Technicians //</h6>
+                <h1 className="mb-5">Our Expert Technicians</h1>
             </div>
-            <div class="row g-4">
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="team-item">
-                        <div class="position-relative overflow-hidden">
-                            <img class="img-fluid" src="assets/img/team-1.jpg" alt="" />
-                            <div class="team-overlay position-absolute start-0 top-0 w-100 h-100">
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-instagram"></i></a>
+            <div className="row g-4">
+                <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div className="team-item">
+                        <div className="position-relative overflow-hidden">
+                            <img className="img-fluid" src="assets/img/team-1.jpg" alt="" />
+                            <div className="team-overlay position-absolute start-0 top-0 w-100 h-100">
+                                <a className="btn btn-square mx-1" href=""><i className="fab fa-facebook-f"></i></a>
+                                <a className="btn btn-square mx-1" href=""><i className="fab fa-twitter"></i></a>
+                                <a className="btn btn-square mx-1" href=""><i className="fab fa-instagram"></i></a>
                             </div>
                         </div>
-                        <div class="bg-light text-center p-4">
-                            <h5 class="fw-bold mb-0">Full Name</h5>
+                        <div className="bg-light text-center p-4">
+                            <h5 className="fw-bold mb-0">Full Name</h5>
                             <small>Designation</small>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="team-item">
-                        <div class="position-relative overflow-hidden">
-                            <img class="img-fluid" src="assets/img/team-2.jpg" alt="" />
-                            <div class="team-overlay position-absolute start-0 top-0 w-100 h-100">
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-instagram"></i></a>
+                <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div className="team-item">
+                        <div className="position-relative overflow-hidden">
+                            <img className="img-fluid" src="assets/img/team-2.jpg" alt="" />
+                            <div className="team-overlay position-absolute start-0 top-0 w-100 h-100">
+                                <a className="btn btn-square mx-1" href=""><i className="fab fa-facebook-f"></i></a>
+                                <a className="btn btn-square mx-1" href=""><i className="fab fa-twitter"></i></a>
+                                <a className="btn btn-square mx-1" href=""><i className="fab fa-instagram"></i></a>
                             </div>
                         </div>
-                        <div class="bg-light text-center p-4">
-                            <h5 class="fw-bold mb-0">Full Name</h5>
+                        <div className="bg-light text-center p-4">
+                            <h5 className="fw-bold mb-0">Full Name</h5>
                             <small>Designation</small>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="team-item">
-                        <div class="position-relative overflow-hidden">
-                            <img class="img-fluid" src="assets/img/team-3.jpg" alt="" />
-                            <div class="team-overlay position-absolute start-0 top-0 w-100 h-100">
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-instagram"></i></a>
+                <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div className="team-item">
+                        <div className="position-relative overflow-hidden">
+                            <img className="img-fluid" src="assets/img/team-3.jpg" alt="" />
+                            <div className="team-overlay position-absolute start-0 top-0 w-100 h-100">
+                                <a className="btn btn-square mx-1" href=""><i className="fab fa-facebook-f"></i></a>
+                                <a className="btn btn-square mx-1" href=""><i className="fab fa-twitter"></i></a>
+                                <a className="btn btn-square mx-1" href=""><i className="fab fa-instagram"></i></a>
                             </div>
                         </div>
-                        <div class="bg-light text-center p-4">
-                            <h5 class="fw-bold mb-0">Full Name</h5>
+                        <div className="bg-light text-center p-4">
+                            <h5 className="fw-bold mb-0">Full Name</h5>
                             <small>Designation</small>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
-                    <div class="team-item">
-                        <div class="position-relative overflow-hidden">
-                            <img class="img-fluid" src="assets/img/team-4.jpg" alt="" />
-                            <div class="team-overlay position-absolute start-0 top-0 w-100 h-100">
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square mx-1" href=""><i class="fab fa-instagram"></i></a>
+                <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
+                    <div className="team-item">
+                        <div className="position-relative overflow-hidden">
+                            <img className="img-fluid" src="assets/img/team-4.jpg" alt="" />
+                            <div className="team-overlay position-absolute start-0 top-0 w-100 h-100">
+                                <a className="btn btn-square mx-1" href=""><i className="fab fa-facebook-f"></i></a>
+                                <a className="btn btn-square mx-1" href=""><i className="fab fa-twitter"></i></a>
+                                <a className="btn btn-square mx-1" href=""><i className="fab fa-instagram"></i></a>
                             </div>
                         </div>
-                        <div class="bg-light text-center p-4">
-                            <h5 class="fw-bold mb-0">Full Name</h5>
+                        <div className="bg-light text-center p-4">
+                            <h5 className="fw-bold mb-0">Full Name</h5>
                             <small>Designation</small>
                         </div>
                     </div>
@@ -458,43 +458,43 @@ export default function Home() {
     {/* <!-- Team End --> */}
 
      {/* <!-- Testimonial Start --> */}
-    <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="container">
-            <div class="text-center">
-                <h6 class="text-primary text-uppercase">// Testimonial //</h6>
-                <h1 class="mb-5">Our Clients Say!</h1>
+    <div className="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div className="container">
+            <div className="text-center">
+                <h6 className="text-primary text-uppercase">// Testimonial //</h6>
+                <h1 className="mb-5">Our Clients Say!</h1>
             </div>
-            <div class="owl-carousel testimonial-carousel position-relative">
-                <div class="testimonial-item text-center">
-                    <img class="bg-light rounded-circle p-2 mx-auto mb-3" src="assets/img/testimonial-1.jpg" style={{width: "80px", height: "80px"}} />
-                    <h5 class="mb-0">Client Name</h5>
+            <div className="owl-carousel testimonial-carousel position-relative">
+                <div className="testimonial-item text-center">
+                    <img className="bg-light rounded-circle p-2 mx-auto mb-3" src="assets/img/testimonial-1.jpg" style={{width: "80px", height: "80px"}} />
+                    <h5 className="mb-0">Client Name</h5>
                     <p>Profession</p>
-                    <div class="testimonial-text bg-light text-center p-4">
-                    <p class="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
+                    <div className="testimonial-text bg-light text-center p-4">
+                    <p className="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
                     </div>
                 </div>
-                <div class="testimonial-item text-center">
-                    <img class="bg-light rounded-circle p-2 mx-auto mb-3" src="assets/img/testimonial-2.jpg" style={{width: "80px", height: "80px"}} />
-                    <h5 class="mb-0">Client Name</h5>
+                <div className="testimonial-item text-center">
+                    <img className="bg-light rounded-circle p-2 mx-auto mb-3" src="assets/img/testimonial-2.jpg" style={{width: "80px", height: "80px"}} />
+                    <h5 className="mb-0">Client Name</h5>
                     <p>Profession</p>
-                    <div class="testimonial-text bg-light text-center p-4">
-                    <p class="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
+                    <div className="testimonial-text bg-light text-center p-4">
+                    <p className="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
                     </div>
                 </div>
-                <div class="testimonial-item text-center">
-                    <img class="bg-light rounded-circle p-2 mx-auto mb-3" src="assets/img/testimonial-3.jpg" style={{width: "80px", height: "80px"}} />
-                    <h5 class="mb-0">Client Name</h5>
+                <div className="testimonial-item text-center">
+                    <img className="bg-light rounded-circle p-2 mx-auto mb-3" src="assets/img/testimonial-3.jpg" style={{width: "80px", height: "80px"}} />
+                    <h5 className="mb-0">Client Name</h5>
                     <p>Profession</p>
-                    <div class="testimonial-text bg-light text-center p-4">
-                    <p class="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
+                    <div className="testimonial-text bg-light text-center p-4">
+                    <p className="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
                     </div>
                 </div>
-                <div class="testimonial-item text-center">
-                    <img class="bg-light rounded-circle p-2 mx-auto mb-3" src="assets/img/testimonial-4.jpg" style={{width: "80px", height: "80px"}} />
-                    <h5 class="mb-0">Client Name</h5>
+                <div className="testimonial-item text-center">
+                    <img className="bg-light rounded-circle p-2 mx-auto mb-3" src="assets/img/testimonial-4.jpg" style={{width: "80px", height: "80px"}} />
+                    <h5 className="mb-0">Client Name</h5>
                     <p>Profession</p>
-                    <div class="testimonial-text bg-light text-center p-4">
-                    <p class="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
+                    <div className="testimonial-text bg-light text-center p-4">
+                    <p className="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
                     </div>
                 </div>
             </div>
