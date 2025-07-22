e1a6ceca2c0d96e1c23aa6307c06e4930bfb3d64
Roshni0511 <roshnichauhan051@gmail.com>
2025-07-22 14:11:19 +0530

    DONE

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -178,27 +178,62 @@ const Services = () => {
     {/* <!-- Services End --> */}
  <div className="call-banner">
       <div className="call-banner-content container">
+         <div class="car-road">
+  <div class="car">
+    <div class="car-top">
+      <div class="window">
+      </div>  
+    </div>  
+    <div class="car-base">
+    </div>
+    <div class="wheel-left wheel">
+       <div class="wheel-spike">
+       </div> 
+       <div class="wheel-center">
+       </div>
+    </div>
+    <div class="wheel-right wheel">
+       <div class="wheel-spike">
+       </div> 
+      <div class="wheel-center">
+       </div>
+    </div>
+    <div class="head-light"></div>
+  </div> 
+  <div class="road">
+  </div>
+</div>  
+        <div className="right-section animated-phone">
+  <p className="small-text">
+ 
+Get an Estimate For Your Service
+  </p>
+<div style={{display:'flex'}}>
+
+  <div className="call-box">
+    <div className="phone-circle">
+      <FaPhoneAlt size={24} />
+    </div>
+    <div className="phone-info">
+      <p style={{ marginBottom: '0px' }}>Call us Now</p>
+      <h4>+0261 266 2999</h4>
+    </div>
+  </div>
+  <div style={{margin:'10px 0px 0px 50px'}}>
+    <a href="/Services" class="btn btn-primary">Discover More</a>
+    
+  </div>
+</div>
+</div>
+
         <div className="left-section">
-          <p className="small-text">
+          {/* <p className="small-text">
             <span className="slashes">///</span> IF ANY CAR RELATED ISSUES ARE BOTHERING
           </p>
-          <h2 className="big-text">GIVE A CALL TO DUCATIBOX</h2>
+          <h2 className="big-text">GIVE A CALL TO DUCATIBOX</h2> */}
         </div>
-   <div className="image-section">
-          <img
-            src="/assets/images/tire-man.png"
-            alt="Tire Man"
-          />
-        </div>
-        <div className="right-section">
-          <div className="phone-circle">
-            <FaPhoneAlt size={24} />
-          </div>
-          <div className="phone-info">
-            <p>We are Available 24x7</p>
-            <h3>+ 455 7878 911 55</h3>
-          </div>
-        </div>
+  
+      
 
      
       </div>
